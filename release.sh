015e165a09c6eb93e4eca66848dbebd2a06cde96
Colin Bate <1060912+colinbate@users.noreply.github.com>
2014-12-13 00:33:15 -0400

    Release v0.3.0

--- release.sh
+++ release.sh
@@ -4,6 +4,6 @@ echo Releasing new $bump version of Planning Poker
 version=$(npm version $bump)
 grunt pack
 echo New version is $version
-echo hg ci -m "Release $version"
-echo hg tag $version -m "Tag for release $version"
-echo rsync -r ./dist/${version#?}/ root@wallboardr.io:/var/apps/pp.cbate.com/
+hg ci -m "Release $version"
+hg tag $version -m "Tag for release $version"
+rsync -r ./dist/${version#?}/ root@wallboardr.io:/var/apps/pp.cbate.com/
