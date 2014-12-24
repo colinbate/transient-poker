193c1ebf0aa0e46ef3abb1e2294c708f27011ca3
Colin Bate <colin@colinbate.com>
2014-12-24 12:13:00 -0400

    Add vote tallying, fixes #3

---
+++ src/modules/helpers.js
@@ -0,0 +1,60 @@
+define([], function () {
+  'use strict';
+  var helper = {},
+      slice = Array.prototype.slice;
+  function appendClass(exist, add) {
+    if (!add) {
+      add = '';
+    }
+    if (!exist) {
+      exist = '';
+    }
+    if (exist && add) {
+      exist += ' ';
+    }
+    return '' + exist + add;
+  }
+  function partialFourth(fn, fourth) {
+    return function (a, b, c) {
+      return fn.call(null, a, b, c, fourth);
+    };
+  }
+  function classy(fn, pos, neg, seed) {
+    var resolver = function () {
+      var args = slice.call(arguments),
+          props = (typeof seed === 'function' ? seed.apply(null, args) : ((typeof seed === 'object' && seed) ? seed : {})),
+          klass = props['class'],
+          toAdd = fn.apply(null, args);
+      if (pos !== undefined || neg !== undefined) {
+        toAdd = toAdd ? pos : neg;
+      }
+      klass = appendClass(klass, toAdd);
+      window.console.log('classy', args.length ? args[0] : 'no args', 'klass', klass);
+      props['class'] = klass;
+      return props;
+    };
+    resolver.and = partialFourth(classy, resolver);
+    resolver.bare = function () {
+      var args = slice.call(arguments),
+          obj  = resolver.apply(null, args);
+      return obj['class'];
+    };
+    return resolver;
+  }
+  helper.classy = classy;
+
+
+  helper.invoke = function (method) {
+    return function () {
+      var args = slice.call(arguments),
+          obj = args.shift();
+      if (method in obj && typeof obj[method] === 'function') {
+        return obj[method].apply(obj, args);
+      }
+    };
+  };
+
+  helper.noop = function () {};
+
+  return helper;
+});
---
+++ src/modules/tally.js
@@ -0,0 +1,38 @@
+define([], function () {
+  'use strict';
+  var tally = {};
+
+  tally.Tally = function () {
+    this.votes = {};
+    this.most = {count: 0, value: null};
+  };
+
+  tally.Tally.prototype.add = function (vote) {
+    this.votes[vote] = (this.votes[vote] || 0) + 1;
+    if (this.votes[vote] > this.most.count) {
+      this.most.count = this.votes[vote];
+      this.most.value = vote;
+    }
+  };
+
+  tally.Tally.prototype.clear = function () {
+    this.votes = {};
+    this.most.count = 0;
+    this.most.value = null;
+  };
+
+  tally.Tally.prototype.getClass = function (vote) {
+    if (vote === null) {
+      return 'no-vote';
+    }
+    if (vote === '?') {
+      return 'vote-pass';
+    }
+    if (vote === this.most.value) {
+      return 'vote-most';
+    }
+    return 'vote-none';
+  };
+
+  return tally;
+});
